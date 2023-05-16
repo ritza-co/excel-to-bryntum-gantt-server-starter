8280866baa97fb12345c82267c65a0f967826de5
Matthew Clarke <m_d_clarke@hotmail.com>
2023-05-16 13:36:19 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5173",
+    credentials: true,
+  })
+);
+
+app.get("/download", async (req, res) => {
+  try {
+    res.json({ success: true });
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).send("An error occurred.");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
